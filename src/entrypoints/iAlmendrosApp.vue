d3c746fc8049b8977549b93d64110c7adbc0be3d
Renato Flores <r.flores@garagroup.com>
2025-09-10 11:15:08 -0600

    Almost done with the boilerplate, but backup before finishing vite + vue configurations

---
+++ src/entrypoints/iAlmendrosApp.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import { onMounted, onUnmounted, ref } from 'vue'
+onMounted(()=>{
+  console.log('Hello, world!')
+})
+
+onUnmounted(()=>{
+  console.log('Cleaning Up!')
+})
+
+</script>
+<template>
+  <div class="w-full">
+    <div class="form-title-splitter" style="display: flex; gap: 15px; align-items: center;">
+      <p>
+        Hello, World!
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped>
+.selected-row {
+  color: white;
+  background: darkblue;
+}
+.non-selected-row {
+  color: inherit;
+  background: inherit;
+}
+.should-expand {
+  cursor: pointer;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+
+import App from './entrypoints/iAlmendrosApp.vue';
+
+createApp(App).mount(`#abcdefgh__klmnopqrstuvwxyz`);
